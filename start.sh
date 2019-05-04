72fdef3d8e962e3f3b3704eef7055a36d375793d
Victor Alessander <alessander@protonmail.com>
2019-05-04 19:46:39 -0300

    adicionada externalizacao

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 export FLASK_APP=main.py
 echo '[+] Environment variable created'
 echo '[+] Starting server'
-FLASK_DEBUG=1 FLASK_ENV=development flask run
+FLASK_DEBUG=1 FLASK_ENV=development flask run --host=0.0.0.0
